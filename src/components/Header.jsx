b22f48e114ca39774d25753b905ee2d883cc71b2
Maksym Morenko <morenko.maxim@gmail.com>
2022-07-05 15:26:53 +0300

    добавляем сслки в шапке на страницы через компонент Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+// чтобы приложение не перезагружалось при смене страниц, используем компонент Link и его атрибут to (пувть) вместо тега а
+import { Link } from 'react-router-dom';
 
 function Header(props) {
     return (
-      // меняем цвет наву, добавляем классНейм наву с названием цвета из палитры в библиотеке https://materializecss.com/color.html
-      <nav className='green darken-2'>
-      <div className='nav-wrapper'>
-          <a href='#!' className='brand-logo'>
-              React Shop
-          </a>
-          <ul id='nav-mobile' className='right hide-on-med-and-down'>
-              <li>
-                  <a href='https://github.com/maximmorenko/spa-routing-project'>
-                      Repo
-                  </a>
-              </li>
-          </ul>
-      </div>
-  </nav>
+        // меняем цвет наву, добавляем классНейм наву с названием цвета из палитры в библиотеке https://materializecss.com/color.html
+        <nav className='green darken-2'>
+            <div className='nav-wrapper'>
+                <Link to='/' className='brand-logo'>
+                    React SPA
+                </Link>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                    <li>
+                        <a href='https://github.com/maximmorenko/spa-routing-project'>Repo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
 
